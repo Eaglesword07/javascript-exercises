701a63499f1240dc799a61f2063aeb8666e5ac44
Eaglesword07 <kennyroger4u@rocketmail.com>
2023-08-19 23:33:12 +0200

    fixed exercise with string method

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function (string, numb) {
+    if (numb < 0) {
+        return "ERROR"
+    } else
+        return string.repeat(numb)
 };
 
 // Do not edit below this line
