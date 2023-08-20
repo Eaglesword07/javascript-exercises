d032d2a098241d939c9afcdb8db3236a00a8a9b6
Eaglesword07 <kennyroger4u@rocketmail.com>
2023-08-20 23:17:46 +0200

    fixed exercise code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+        const stringArr = str.split("");
+        const reverseStringArr = stringArr.reverse();
+        const newStr = reverseStringArr.join("");
+        return newStr;
 };
 
 // Do not edit below this line
