832de4e4ef73bba56a7d11a99f06cbeff3f04988
Eaglesword07 <kennyroger4u@rocketmail.com>
2023-09-10 20:54:37 +0200

    solved using the foreach method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...value) {
 
+    // first we crreat an empty array
+    const newArr = [];
+
+    // now, we use the .foreach() method to go through the array
+    arr.forEach(
+        // function that goes through the array and pushes every element
+        // into the array except its included in the initial ...value argument
+        (figure) => {
+            if (!value.includes(figure)) {
+                newArr.push(figure);
+            }
+        } 
+    )
+    // return the new array
+    return newArr;
 };
 
 // Do not edit below this line
