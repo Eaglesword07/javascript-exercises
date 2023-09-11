5f64940b11826ae70d46feaaee032c292696b274
Eaglesword07 <kennyroger4u@rocketmail.com>
2023-09-11 15:21:50 +0200

    completed exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(arg) {
 
+    // check arg to find out if its a leap year following the requirements
+    // using the modulo operator works fine with this
+
+    if ((arg % 4 === 0 && arg % 100 !== 0) ||
+    (arg % 400 === 0)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
