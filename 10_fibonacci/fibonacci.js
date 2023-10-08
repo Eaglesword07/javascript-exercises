4b40c925414e948096b40d9b9e943830fbab03ba
Eaglesword07 <kennyroger4u@rocketmail.com>
2023-10-08 23:36:03 +0200

    completed exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    if (num == 1) {
+        return 1;
+    } else if (num < 1) {
+        return 'OOPS'
+    } else {
+        let fnum0 = 0;
+        let fnum1 = 1;
+        let nextfnum;
+        for (let i = 1; i < num; i++) {
+            nextfnum = fnum0 + fnum1;
+            fnum0 = fnum1;
+            fnum1 = nextfnum;
+        }
+        return nextfnum;
+    }
 };
 
 // Do not edit below this line
