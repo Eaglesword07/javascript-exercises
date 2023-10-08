4ab741ceb3d11b569115a06d1954b7e2bfc96410
Eaglesword07 <kennyroger4u@rocketmail.com>
2023-10-08 23:30:45 +0200

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Remove punctuation from string using RegExp 
+    const re = /[^A-Za-z0-9]/g;
 
+    // convert them all to lower case and replace the punctuation
+    const lowCaseStr = str.toLowerCase().replace(re, '');
+    
+    // get new string by spliting original string into an array
+    // reversing that and joining it back together
+    const newStr = lowCaseStr.split('').reverse().join('');
+
+    // Compare new string with lowercase string
+   return lowCaseStr === newStr;
 };
 
 // Do not edit below this line
