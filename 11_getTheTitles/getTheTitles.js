8fd134341218ffd1bcc5edd17828c1880578f7c3
Eaglesword07 <kennyroger4u@rocketmail.com>
2023-10-10 23:43:46 +0200

    we can either loop through or use the shorter map() array method

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,15 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    let titles = [];
+    for (let i = 0; i < books.length; ++i) {
+        titles[i] = books[i].title;
+    }
+    return titles;
 };
+
+// we could as well use the array method map(), which of course is a shorter code
+// const getTheTitles = function(books){
+//     return books.map((book)=> book.title); // returns an array with all title values from each object in 'books' parameter, using
+//         };
 
 // Do not edit below this line
 module.exports = getTheTitles;
